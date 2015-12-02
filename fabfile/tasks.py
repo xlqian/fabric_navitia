45b382fb4df9a21c9b23ae2f8f82f88477749e3b
Hoshiyo <aguyen89@gmail.com>
2015-12-02 15:06:17 +0100

    bypass for tyr_beat

--- fabfile/tasks.py
+++ fabfile/tasks.py
@@ -250,7 +250,10 @@ def upgrade_tyr(up_confs=False, pilot_tyr_beat=True):
     execute(tyr.upgrade_tyr_packages)
     execute(tyr.setup_tyr_master)
     execute(tyr.upgrade_ed_packages)
-    execute(tyr.upgrade_db_tyr)
+    if pilot_tyr_beat:
+        execute(tyr.upgrade_db_tyr)
+    else:
+        execute(tyr.upgrade_db_tyr, pilot_tyr_beat=False)
     if up_confs:
         tyr.update_tyr_confs()
     if pilot_tyr_beat:
